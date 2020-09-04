e793971aa6e5467fcb1313d9f7d2b8adc0fd9849
jackismoe <jacksonmo@me.com>
2020-09-04 11:05:56 -0400

    Resubmit

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -1,11 +1,18 @@
 class BirdsController < ApplicationController
   def index
     birds = Bird.all
-    render json: birds, only: [:id, :name, :species] # or except: [:created_at, :updated_at]
+    # render json: birds, except: [:created_at, :updated_at]
+    render json: birds.to_json(except: [:created_at, :updated_at])
   end
 
   def show
     bird = Bird.find_by(id: params[:id])
-    render json: bird
+    # render json: {id: bird.id, name: bird.name, species: bird.species}
+    # render json: bird.slice(:id, :name, :species) (doesnt work for an array of hashes)
+    if bird
+      render json: bird, only: [:id, :name, :species]
+    else
+      render json: {message: 'Bird not found'}
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,3 @@
 Rails.application.routes.draw do
-  get '/birds' => 'birds#index'
-  get '/birds/:id' do: 'birds#show'
+  resources :birds, only: [:index, :show]
 end
